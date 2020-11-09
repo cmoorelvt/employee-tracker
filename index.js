648c1cab3b4ea8d980c5cc2ad5f66b9f1bbf828e
Christine Moore <cmoorelvt@gmail.com>
2020-11-09 14:19:54 -0500

    view all emp by mangr

--- index.js
+++ index.js
@@ -151,3 +151,38 @@ function viewAllEmpByDept() {
             });
         });
 }
+
+function viewAllEmpByMan() {
+    let managerArr = [];
+    promisemysql.createConnection(connectionProperties)
+    .then((conn) => {
+        return conn.query("SELECT DISTINCT m.id, CONCAT(m.first_name, ' ', m.last_name) AS manager FROM employee e INNER JOIN employee m ON e.manager_id = m.id");
+    }).then(function(managers) {
+        for (i = 0; i < managers.length; i++) {
+            managerArr.push(managers[i].manager);
+        }
+        return managers;
+    }).then((managers) => {
+        inquirer.prompt({
+            name: "manager",
+            type: "list",
+            message: "Which manager would oyu like to search?",
+            choices: managerArr
+        })
+        .then((answer) => {
+            let managerID;
+            for (i = 0; i < managers.length; i++){
+                if (answer.manager == managers[i].manager){
+                    managerID = managers[i].id;
+                }
+            }
+            const query = `SELECT e.id, e.first_name, e.last_name, role.title, department.name AS department, role.salary, concat(m.first_name, ' ' ,  m.last_name) AS manager from employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE e.manager_id = ${managerID};`;
+            connection.query(query, (err, res) => {
+                if (err) return err;
+                console.log("\n");
+                console.table(res);
+                mainMenu();
+            });
+        });
+    });
+}
