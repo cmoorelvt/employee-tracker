45d20ea9b238ba3e0f5dd364d512fe5e2ec25691
Christine Moore <cmoorelvt@gmail.com>
2020-11-09 14:07:23 -0500

    fixed closers

--- index.js
+++ index.js
@@ -134,15 +134,13 @@ function viewAllEmpByDept() {
                 for (i = 0; i < value.length; i++){
                     deptArr.push(value[i].name);
                 }
-            })
-            .then(() => {
+            }).then(() => {
                 inquirer.prompt({
                     name: "department",
                     type: "list",
                     message: "Which dept would you like to search for?",
                     choices: deptArr
-                })
-            }).then((answer) => {
+                }).then((answer) => {
                 const query = `SELECT e.id AS ID, e.first_name AS 'First Name', e.last_name AS 'Last Name', role.title AS Title, department.name AS Department, role.salary AS Salary, concat(m.first_name, ' ' ,  m.last_name) AS Manager FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id WHERE department.name = '${answer.department}' ORDER BY ID ASC`;
                 connection.query(query, (err, res) => {
                     if(err) return err;
@@ -151,4 +149,5 @@ function viewAllEmpByDept() {
                     mainMenu();
                 });
             });
+        });
 }
