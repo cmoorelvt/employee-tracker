ae71dd37148844acc39d7bdcbf0d8425384aed27
Christine Moore <cmoorelvt@gmail.com>
2020-11-10 09:33:09 -0500

    bulk is there

--- index.js
+++ index.js
@@ -523,3 +523,88 @@ function remRole() {
         })
     });
 }
+
+function remDept() {
+    let deptArr = [];
+    promisemysql.createConnection(connectionProperties)
+    .then((conn) => {
+        return conn.query("SELECT id, name FROM department");
+    }).then((depts) => {
+        for (i=0; i < depts.length; i++){
+            deptArr.push(depts[i].name);
+        }
+        inquirer.prompt([
+            {
+                name: "continueRemove",
+                type: "list",
+                message: "Removing a department will remove all roles and employees associated with the department. Do you want to continue?",
+                choices: ["NO", "YES"]
+            }
+        ]).then((answer) => {
+            if (answer.continueRemove === "NO") {
+                mainMenu();
+            }
+        }).then(() => {
+            inquirer.prompt([
+                {
+                    name: "dept",
+                    type: "list",
+                    message: "Which dept would you like to remove?",
+                    choices: deptArr 
+                },
+                {
+                    name: "confirmRemove",
+                    type: "Input",
+                    message: "Please type the dept to confirm removal." 
+                }
+            ]).then((answer) => {
+                if(answer.confirmRemove === answer.dept){
+                    let deptID;
+                    for (i=0; i < depts.length; i++){
+                        if (answer.dept == depts[i].name){
+                            deptID = depts[i].id;
+                        }
+                    }
+                    connection.query(`DELETE FROM department WHERE id=${deptID};`, (err, res) => {
+                        if(err) return err;
+                        console.log(`\n DEPARTMENT '${answer.dept}' DELETED...\n `);
+                        mainMenu();
+                    });
+                }
+                else {
+                    console.log(`\n DEPARTMENT '${answer.dept}' NOT DELETED...\n `);
+                    mainMenu();
+                }
+            });    
+        })
+    });
+}
+
+function viewDeptBudget() {
+    promisemysql.createConnection(connectionProperties)
+    .then((conn) => {
+        return  Promise.all([
+            conn.query("SELECT department.name AS department, role.salary FROM employee e LEFT JOIN employee m ON e.manager_id = m.id INNER JOIN role ON e.role_id = role.id INNER JOIN department ON role.department_id = department.id ORDER BY department ASC"),
+            conn.query('SELECT name FROM department ORDER BY name ASC')
+        ]);
+    }).then(([deptSalaies, departments]) => {
+        let deptBudgetArr =[];
+        let department;
+        for (d=0; d < departments.length; d++){
+            let departmentBudget = 0;
+            for (i=0; i < deptSalaies.length; i++){
+                if (departments[d].name == deptSalaies[i].department){
+                    departmentBudget += deptSalaies[i].salary;
+                }
+            }
+            department = {
+                Department: departments[d].name,
+                Budget: departmentBudget
+            }
+            deptBudgetArr.push(department);
+        }
+        console.log("\n");
+        console.table(deptBudgetArr);
+        mainMenu();
+    }); 
+}    
